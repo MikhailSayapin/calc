36802ae1fcd3899f8096fba618062354b49847ab
MikhailSayapin <32384891+MikhailSayapin@users.noreply.github.com>
2017-10-04 22:50:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,59 +6,70 @@
 using namespace std;
 int main(int argc, char* argv[])
 {
-  cout<<"z:\t"<<argc-1<<endl;
-  for( int a=1; a<=argc-1; a++)
-  {
-      cout <<"argv\t["<<a<<"]\t=\t"<< argv[a]<<endl;}
-{
-  char b;
-  b=argv[2][0];
-  double x , y;
-  x=  atof (argv[1]);
-  y=  atof (argv[3]);
-  switch (b)
-{
-        case '+':
-       {
-           cout << x<< "+" <<y<< "=" << x+y;
-       break;}
-            case '-':
-       {
-           cout << x<< "-"<<y<< "="<< x-y;
-       break;}
-            case '*':
-       {
-           cout << x<< "*"<< y<< "="<< x*y;
-       break;}
-           case '%':
-       {
-           cout << x<< "%"<< y<< "="<< (int)x % (int)y;
-       break;}
-           case '/':
-       {
-           cout << x<< "/"<< y<< "="<< x/y;
-       break;}
-           case '&':
-       {
-           
-           cout << x<< "&"<< y<< "="<< x&&y;
-       break;}
-           case 'p': /*p - значит возведение в степень. */ 
-        {
-            cout << x<< "v stepeni"<< y<< "="<< pow (x,y) ;
-        break;}
-           case '^':
-       {
-           cout << x<< "^"<< y<< "="<< x||y ;
-        break;}
-           case 'c': /* c - значит сравнение. */
-        {
-            if (x<y) cout << "x<y" ;
-            if (x>y) cout << "x>y" ;
-            else cout << "x==y" ;
-       break;}
-      default:
-      cout<<"err";}
-      system("pause");
-      return 0;}
+    cout << "z:\t" << argc - 1 << endl;
+    for (int a = 1; a <= argc - 1; a++) {
+        cout << "argv\t[" << a << "]\t=\t" << argv[a] << endl;
+    }
+    {
+        char b;
+        b = argv[2][0];
+        double x, y;
+        x = atof(argv[1]);
+        y = atof(argv[3]);
+        if (argc == 3) {
+            switch (b) {
+            case '+': {
+                cout << x << "+" << y << "=" << x + y;
+                break;
+            }
+            case '-': {
+                cout << x << "-" << y << "=" << x - y;
+                break;
+            }
+            case '*': {
+                cout << x << "*" << y << "=" << x * y;
+                break;
+            }
+            case '%': {
+                cout << x << "%" << y << "=" << (int)x % (int)y;
+                break;
+            }
+            case '/': {
+                if (y == 0)
+                    cout << "err";
+                else
+                    cout << x << "/" << y << "=" << x / y;
+                break;
+            }
+            case '&': {
+                cout << x << "&" << y << "=" << x && y;
+                break;
+            }
+            case 'p': /*p - значит возведение в степень. */
+            {
+                cout << x << "v stepeni" << y << "=" << pow(x, y);
+                break;
+            }
+            case '^': {
+                cout << x << "^" << y << "=" << x || y;
+                break;
+            }
+            case 'c': /* c - значит сравнение. */
+            {
+                if (x < y)
+                    cout << "x<y";
+                if (x > y)
+                    cout << "x>y";
+                else
+                    cout << "x==y";
+                break;
+            }
+            default:
+                cout << "err";
+            }
+        }
+        else
+            cout << "err";
+    }
+    return 0;
 }
