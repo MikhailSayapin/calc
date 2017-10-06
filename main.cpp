fb65760419d65505d5626309afa3450e0f1601cd
MikhailSayapin <32384891+MikhailSayapin@users.noreply.github.com>
2017-10-07 02:52:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,96 +8,99 @@ int main()
     int x;
     int y;
     char d;
-    cout << "Enter the operation:\n +, -, *, /, &, |, ^ , c, <, >, !, %\n";
-    cin >> d;
-    cout << ("first number\t");
-    cin >> (x);
-    if (d != '!')
+    while (true)
     {
-        cout << ("second number\t");
-        cin >> (y);
-    }
-    switch (d)
-    {
-        case '+':
+        cout << "\nEnter the operation:\n +, -, *, /, &, |, ^ , c, <, >, !, %\n";
+        cin >> d;
+        cout << ("first number\t");
+        cin >> (x);
+        if (d != '!')
         {
-            cout << x << "+" << y << "=" << x + y;
-            break;
+            cout << ("second number\t");
+            cin >> (y);
         }
-        case '-':
+        switch (d)
         {
-            cout << x << "-" << y << "=" << x - y;
-            break;
-        }
-        case '*':
-        {
-            cout << x << "*" << y << "=" << x * y;
-            break;
-        }
-        case '/':
-        {
-            if (y == 0)
-                cout << "err: you can't divide by zero";
-            else
-                cout << x << "/" << y << "=" << (double)(x / y);
-            break;
-        }
-        case '&':
-        {
-            cout << x << "&" << y << "=" << x && y;
-            break;
-        }
-        case '^':
-        {
-            cout << x << "^" << y << "=" << pow(x, y);
-            break;
-        }
-        case '|':
-        {
-            cout << x << "|" << y << "=" << x || y;
-            break;
-        }
-        case 'c': /* c - значит сравнение. */
-        {
-            if (x < y)
-                cout << "x<y";
-            if (x > y)
-                cout << "x>y";
-            else
-                cout << "x==y";
-            break;
-        }
-        case '<':
-        {
-            if (y >= 0)
-                cout << x << "<" << y << "=" << (x << y);
-            else
+            case '+':
+            {
+                cout << x << "+" << y << "=" << x + y;
+                break;
+            }
+            case '-':
+            {
+                cout << x << "-" << y << "=" << x - y;
+                break;
+            }
+            case '*':
+            {
+                cout << x << "*" << y << "=" << x * y;
+                break;
+            }
+            case '/':
+            {
+                if (y == 0)
+                    cout << "err: you can't divide by zero";
+                else
+                    cout << x << "/" << y << "=" << (double)(x / y);
+                break;
+            }
+            case '&':
+            {
+                cout << x << "&" << y << "=" << x && y;
+                break;
+            }
+            case '^':
+            {
+                cout << x << "^" << y << "=" << pow(x, y);
+                break;
+            }
+            case '|':
+            {
+                cout << x << "|" << y << "=" << x || y;
+                break;
+            }
+            case 'c': /* c - значит сравнение. */
+            {
+                if (x < y)
+                    cout << "x<y";
+                if (x > y)
+                    cout << "x>y";
+                else
+                    cout << "x==y";
+                break;
+            }
+            case '<':
+            {
+                if (y >= 0)
+                    cout << x << "<" << y << "=" << (x << y);
+                else
+                    cout << "err";
+                break;
+            }
+            case '>':
+            {
+                if (y >= 0)
+                    cout << x << ">" << y << "=" << (x >> y);
+                else
+                    cout << "err";
+                break;
+            }
+            case '!':
+            {
+                cout << x << "!\t=" << (!x);
+                break;
+            }
+            case '%':
+            {
+                int m;
+                cout << "enter the module";
+                cin >> m;
+                cout << x << "-%" << y << "=" << (x - y) % m;
+                break;
+            }
+            default:
                 cout << "err";
-            break;
         }
-        case '>':
-        {
-            if (y >= 0)
-                cout << x << ">" << y << "=" << (x >> y);
-            else
-                cout << "err";
-            break;
-        }
-        case '!':
-        {
-            cout << x << "!\t=" << (!x);
-            break;
-        }
-        case '%':
-        {
-            int m;
-            cout << "enter the module";
-            cin >> m;
-            cout << x << "-%" << y << "=" << (x - y) % m;
-            break;
-        }
-        default:
-            cout << "err";
     }
     return 0;
 }
